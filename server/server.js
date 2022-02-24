cf2e16d28d809099a9ae6c9954cff5e7f5b7abfc
Cole Henderson <darkgale93@gmail.com>
2022-02-24 12:11:40 -0700

    rollbar

--- server/server.js
+++ server/server.js
@@ -2,7 +2,18 @@ const express = require('express');
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 4545
+//-----------------------------
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '7ff54d89a58440e3b01e2fbc4156043c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+//-----------------------------
 app.use(express.static(path.join(__dirname,'../public')))
 
 app.get('/',(req,res)=>{
