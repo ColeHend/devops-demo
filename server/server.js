a3925d24580836ed04cf0bcf967f710faf4f1c22
Cole Henderson <darkgale93@gmail.com>
2022-02-24 12:36:38 -0700

    message

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 4545
-//-----------------------------
+
+//--------------Rollbar---------------
 // include and initialize the rollbar library with your access token
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
@@ -14,11 +15,26 @@ const rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 //-----------------------------
+
 app.use(express.static(path.join(__dirname,'../public')))
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
+
+let students = []
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('student was added successfully',{author:'Cole',type:'manual',student: name})
+
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
